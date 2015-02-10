250c6e70daa3836a2741aed0fe34f9e1b2d61f12
Ben Moore <b3nmoore@gmail.com>
2015-02-10 15:21:12 +0000

    bioinformatics misc

---
+++ R/bioinfo_fns.R
@@ -0,0 +1,17 @@
+#' Write data.frame in BED format
+#' 
+#' Sets options to `write.table` useful for
+#' writing BED files.
+#' 
+#' @export
+#' @param df data.frame (typically 4+ column)
+#' @param fname filename to save BED file
+#' 
+#' @examples
+#' write_bed(data.frame(chr=c("chr1", "chr2"),
+#'   start=c(100, 450), end=c(200, 300), id=c(p1, p2)),
+#'   "outfile.bed")
+#'   
+write_bed <- function(df, fname)
+  write.table(df, fname, quote=F, row.names=F,
+              col.names=F, sep="\t")
